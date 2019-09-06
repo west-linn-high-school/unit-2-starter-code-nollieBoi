42969ebc89629f8f4633707c10faf9b23f3fa8b8
Michael George <georgem@wlwv.k12.or.us>
2019-09-06 09:15:24 -0700

    Signed-off-by: Michael George <georgem@wlwv.k12.or.us>

---
+++ Jabberwocky.java
@@ -0,0 +1,15 @@
+public class Jabberwocky{
+    public static void main(String[] args){
+        System.out.print("'Twas brillig, and the slithy toves/\n");
+        System.out.print("Did gyre and gimble in the wabe:\n");
+        System.out.print("All mimsy were the \"borogroves\"/\n");
+        System.out.print("And the mome raths out grabe.");
+    }
+}
+
+/** 
+ * ’twas brillig, and the slithy toves/
+ * Did gyre and gimble in the wabe:
+   All mimsy were the “borogroves”/
+   And the mome raths out grabe.   
+ */
