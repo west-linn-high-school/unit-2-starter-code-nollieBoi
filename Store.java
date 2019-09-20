3c8b4160ecf27caa321ede5b86113111cef88859
Nolan Kohler <kohlern@wlha.wlwv.k12.or.us>
2019-09-20 09:24:28 -0700

    work

--- Store.java
+++ Store.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.text.DecimalFormat;
 public class Store {
   public static void main (String[] args){
     Scanner input= new Scanner(System.in);
+    DecimalFormat fmt=new DecimalFormat("0.00");
     int bookNumber;
     System.out.println("Number of books: ");
     bookNumber=input.nextInt();
@@ -10,7 +12,7 @@ public class Store {
     bookCost=bookNumber*9.00;
     double bookShipping;
     final double SHIPPINGBOOKCOST=0.95;
-    bookShipping=bookCost*0.95;
+    bookShipping=bookNumber*0.95;
 
     double peanutPounds;
     System.out.println("Pounds of peanuts: ");
@@ -24,31 +26,47 @@ public class Store {
 
     int movieNumber;
     System.out.println("Number of movies: ");
+
     movieNumber=input.nextInt();
     double movieCost;
     final double COSTMOVIE=13.97;
     movieCost=movieNumber*13.97;
     double movieShipping;
     movieShipping=movieCost*0.04;
-    ////////////////////////////////////////
-    System.out.println("Steve's S-Mart");
+    //printing of actual recipt
+    System.out.println("\nSteve's S-Mart");
     System.out.println("Served by: Roxie");
     int dateMonth;
     dateMonth= (int) (Math.random()*12+1);
     int dateDay;
     dateDay= (int)(Math.random ()*28 +1);
+    int timeHour;
     timeHour= (int)(Math.random()*24);
+    int timeMinute;
+    timeMinute= (int)(Math.random()*60);
+    int timeSecond;
+    timeSecond= (int)(Math.random()*60);
 
-    System.out.println("Date:"+ dateMonth  "Time:");
-    
+
+    System.out.println("Date: "+ dateMonth + "/" + dateDay + "/2019");
+    System.out.println("Time: " + (timeHour) +":"+ (timeMinute) + ":"+ timeSecond);
+    System.out.println("\n" + bookNumber + " Books: $ " + fmt.format(bookCost));
+    fmt.applyPattern("0.#");
+    System.out.print (fmt.format(peanutPounds));
+    fmt.applyPattern("0.00");
+    System.out.println( " Peanuts: $ "+ fmt.format(peanutCost));
+    System.out.println(movieNumber + " Movie: $ " + fmt.format(movieCost));
+    System.out.println("-------------------------------");
     double taxes;
     taxes=0.072*(movieCost+bookCost);
+    System.out.println("Taxes: $ " + fmt.format(taxes));
     double totalShipping;
     totalShipping=movieShipping+bookShipping+peanutShipping;
-    System.out.println("Shipping" + totalShipping);
+    System.out.println("Shipping: $ " + fmt.format(totalShipping));
     double grandTotal;
     grandTotal= movieCost+bookCost+peanutCost+totalShipping+taxes;
-    System.out.println("Grand Total" + grandTotal);
+    System.out.println("Grand Total: $ " + fmt.format(grandTotal));
+    System.out.println("\nIt's always a breeze with Steve!");
 }
 
 
