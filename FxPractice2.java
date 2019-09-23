7d83bc48228c82b9025e47ef6dd33093bdc93c9b
Nolan Kohler <kohlern@wlha.wlwv.k12.or.us>
2019-09-23 09:24:21 -0700

    work

---
+++ FxPractice2.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Line;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class FxPractice2 extends Application {
+    public void start(Stage stage){
+      Text greeting = new Text("Howdy gamers! How's the gaming going today?");
+      Ellipse shape = new Ellipse(12.0, 20.0, 5.0, 7.0);
+      Rectangle box = new Rectangle();
+      Line line = new Line();
+
+    }
+
+}
